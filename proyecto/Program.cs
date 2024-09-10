a6c42a701994d91d04615598bed116c207da5224
Mel <150834946+MelsCrest@users.noreply.github.com>
2024-09-10 19:00:14 +0000

    primeras lineas

---
+++ proyecto/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.BackgroundColor = ConsoleColor.Cyan; //va primero porque sino no se sabe a qué se aplica el color
+Console.WriteLine("Hello, World!");
+
